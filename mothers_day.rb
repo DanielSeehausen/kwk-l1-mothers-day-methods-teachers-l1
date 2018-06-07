4efbe5731ba465105f7815ed8542213aab9b602d
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-07 16:14:03 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,5 +1,8 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day() 
+  
+end
 
 
 ## Call your method below - use puts to see it in the terminal!
