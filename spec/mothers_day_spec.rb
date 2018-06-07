37f47b212ffef7af85f97c4d77cfa2b85171ba17
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-07 16:22:26 +0000

    automatically backed up by learn

--- spec/mothers_day_spec.rb
+++ spec/mothers_day_spec.rb
@@ -4,7 +4,7 @@ require_relative './spec_helper'
 describe "crazy_string.rb" do
 
   it 'accepts an argument of a name and prints out "Happy Mother\'s Day, <name>!"' do
-    expect(mothers_day("Tracy")).to eq("Happy mothers day, Tracy!")
+    expect(mothers_day("Tracy")).to eq("Happy Mother's Day, Tracy!")
     mothers_day("Tracy")
   end
     
