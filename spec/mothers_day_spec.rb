666e6bc89bf6a37d0baf2b3ec25acf0b89ed3102
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-07 16:21:24 +0000

    automatically backed up by learn

--- spec/mothers_day_spec.rb
+++ spec/mothers_day_spec.rb
@@ -3,18 +3,14 @@ require_relative './spec_helper'
 
 describe "crazy_string.rb" do
 
-  describe "#mothers_day" do
-
-    it 'accepts an argument of a name and prints out "Happy mothers day, <name>!"' do
-      expect { mothers_day("Tracy") }.to eq("Happy mothers day, Tracy!")
-      mothers_day("Tracy")
-    end
-      
-    it 'defaults to Mom when no name is passed in' do
-      expect($stdout).to receive(:puts).with("Happy mothers day, Mom! I hope you have a great day!")
-      mothers_day()
-    end
-    
+  it 'accepts an argument of a name and prints out "Happy mothers day, <name>!"' do
+    expect { mothers_day("Tracy") }.to eq("Happy mothers day, Tracy!")
+    mothers_day("Tracy")
   end
-
+    
+  it 'defaults to Mom when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Happy mothers day, Mom! I hope you have a great day!")
+    mothers_day()
+  end
+  
 end
